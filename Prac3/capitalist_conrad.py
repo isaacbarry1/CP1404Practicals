ab563130d414eeb09f6a54f47bef2968a07bc858
Isaac 2.0 <isaac.barry@my.jcu.edu.au>
2022-10-02 19:28:32 +1000

    Added F formatting and OUTPUT_FILE.txt

--- Prac3/capitalist_conrad.py
+++ Prac3/capitalist_conrad.py
@@ -1,25 +1,31 @@
 """
 CP1404/CP5632 - Practical
 Capitalist Conrad wants a stock price simulator for a volatile stock.
-The price starts off at $10.00, and, at the end of every day there is
-a 50% chance it increases by 0 to 10%, and
+The price starts off at $1.00, and, at the end of every day there is
+a 50% chance it increases by 0 to 12%, and
 a 50% chance that it decreases by 0 to 5%.
-If the price rises above $1000, or falls below $0.01, the program should end.
+If the price rises above $100, or falls below $1.0, the program should end.
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.12  # 12%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
-INITIAL_PRICE = 10.0
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
+INITIAL_PRICE = 1.0
+OUTPUT_FILE = "OUTPUT_FILE.txt"
+
+# Output file
+out_file = open(OUTPUT_FILE, 'w')
 
 price = INITIAL_PRICE
-print("${:,.2f}".format(price))
+day = 0
+print(f"Starting price: ${price:.2}", file=out_file)
 
 while price >= MIN_PRICE and price <= MAX_PRICE:
     price_change = 0
+    day += 1
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
@@ -32,4 +38,8 @@ while price >= MIN_PRICE and price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print("${:,.2f}".format(price))
+    print(f"On day {day} price is: ${price:.2f}", file=out_file)
+
+
+# Closing file
+
