6491b66b37f4ad412fceaae04002f7494346a227
IsaacBarry <isaac.barry@my.jcu.edu.au>
2022-10-12 20:02:46 +1000

    Completed Warmup

---
+++ Prac4/lists_warmup.py
@@ -0,0 +1,20 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0] = 3
+# numbers[-1] = 2
+# numbers[3]= 1
+# numbers[:-1] = 3 1 4 1 5 9
+# numbers[3:4] = 1 5
+# 5 numbers = numbers[4]
+# 7 in numbers = numbers[0] + numbers[2]
+# 3 in numbers = str(numbers[0])
+# numbers + [6, 5, 3] = numbers + [6, 5, 3]
+# change the first element of numbers to ten
+numbers[0] = "ten"
+# change the last element of numbers to 1
+numbers[-1] = 1
+# print all the elements from numbers except the first two
+print(numbers[2:])
+# check if 9 is an element of numbers
+9 in numbers
+
+
