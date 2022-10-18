1d288efbb0ff6d895d3e3289809af1488bc37743
Isaac 2.0 <isaac.barry@my.jcu.edu.au>
2022-10-18 11:40:09 +1000

    .

--- Prac5/states_names.py
+++ Prac5/states_names.py
@@ -4,9 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-
-import pprint
-
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales",
                 "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria",
